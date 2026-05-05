b12dfef6a10d582455462383d80a6267d9c1715a
notsig4bbr-glitch <notsig4bbr@gmail.com>
2026-05-05 15:11:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -44,4 +44,6 @@ async def websocket_endpoint(websocket: WebSocket, username: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Railway передает порт через переменную PORT
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
